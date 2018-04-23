40c563c8e32045ef38bd4bdb7fb5836f507045fe
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WindowsApplication1/Form1.Designer.cs
@@ -0,0 +1,172 @@
+namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.gridColumnFirstFile = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.repositoryItemMemoEdit1 = new DevExpress.XtraEditors.Repository.RepositoryItemMemoEdit();
+            this.gridColumnLineNumber = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumnSecondFile = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridView2 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemMemoEdit1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 0);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.repositoryItemMemoEdit1});
+            this.gridControl1.Size = new System.Drawing.Size(945, 663);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1,
+            this.gridView2});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.gridColumnFirstFile,
+            this.gridColumnLineNumber,
+            this.gridColumnSecondFile});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            this.gridView1.OptionsSelection.EnableAppearanceFocusedCell = false;
+            this.gridView1.OptionsSelection.EnableAppearanceFocusedRow = false;
+            this.gridView1.OptionsSelection.EnableAppearanceHideSelection = false;
+            this.gridView1.OptionsSelection.UseIndicatorForSelection = false;
+            this.gridView1.OptionsView.RowAutoHeight = true;
+            this.gridView1.OptionsView.ShowGroupPanel = false;
+            this.gridView1.OptionsView.ShowHorizontalLines = DevExpress.Utils.DefaultBoolean.False;
+            this.gridView1.OptionsView.ShowIndicator = false;
+            this.gridView1.RowStyle += new DevExpress.XtraGrid.Views.Grid.RowStyleEventHandler(this.gridView1_RowStyle);
+            this.gridView1.CustomColumnDisplayText += new DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventHandler(this.gridView1_CustomColumnDisplayText);
+            // 
+            // gridColumnFirstFile
+            // 
+            this.gridColumnFirstFile.Caption = "File 1";
+            this.gridColumnFirstFile.ColumnEdit = this.repositoryItemMemoEdit1;
+            this.gridColumnFirstFile.FieldName = "FirstFileLine";
+            this.gridColumnFirstFile.Name = "gridColumnFirstFile";
+            this.gridColumnFirstFile.Visible = true;
+            this.gridColumnFirstFile.VisibleIndex = 0;
+            this.gridColumnFirstFile.Width = 259;
+            // 
+            // repositoryItemMemoEdit1
+            // 
+            this.repositoryItemMemoEdit1.Name = "repositoryItemMemoEdit1";
+            // 
+            // gridColumnLineNumber
+            // 
+            this.gridColumnLineNumber.AppearanceCell.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.gridColumnLineNumber.AppearanceCell.BackColor2 = System.Drawing.Color.Silver;
+            this.gridColumnLineNumber.AppearanceCell.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.gridColumnLineNumber.AppearanceCell.GradientMode = System.Drawing.Drawing2D.LinearGradientMode.Vertical;
+            this.gridColumnLineNumber.AppearanceCell.Options.UseBackColor = true;
+            this.gridColumnLineNumber.AppearanceCell.Options.UseFont = true;
+            this.gridColumnLineNumber.AppearanceCell.Options.UseTextOptions = true;
+            this.gridColumnLineNumber.AppearanceCell.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.gridColumnLineNumber.Caption = "Line number";
+            this.gridColumnLineNumber.FieldName = "gridColumnLineNumber";
+            this.gridColumnLineNumber.Name = "gridColumnLineNumber";
+            this.gridColumnLineNumber.OptionsColumn.AllowEdit = false;
+            this.gridColumnLineNumber.OptionsColumn.AllowFocus = false;
+            this.gridColumnLineNumber.OptionsColumn.AllowGroup = DevExpress.Utils.DefaultBoolean.False;
+            this.gridColumnLineNumber.OptionsColumn.AllowMove = false;
+            this.gridColumnLineNumber.OptionsColumn.AllowSort = DevExpress.Utils.DefaultBoolean.False;
+            this.gridColumnLineNumber.OptionsColumn.FixedWidth = true;
+            this.gridColumnLineNumber.OptionsColumn.ReadOnly = true;
+            this.gridColumnLineNumber.UnboundType = DevExpress.Data.UnboundColumnType.Integer;
+            this.gridColumnLineNumber.Visible = true;
+            this.gridColumnLineNumber.VisibleIndex = 1;
+            this.gridColumnLineNumber.Width = 95;
+            // 
+            // gridColumnSecondFile
+            // 
+            this.gridColumnSecondFile.Caption = "File 2";
+            this.gridColumnSecondFile.ColumnEdit = this.repositoryItemMemoEdit1;
+            this.gridColumnSecondFile.FieldName = "SecondFileLine";
+            this.gridColumnSecondFile.Name = "gridColumnSecondFile";
+            this.gridColumnSecondFile.Visible = true;
+            this.gridColumnSecondFile.VisibleIndex = 2;
+            this.gridColumnSecondFile.Width = 266;
+            // 
+            // gridView2
+            // 
+            this.gridView2.GridControl = this.gridControl1;
+            this.gridView2.Name = "gridView2";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 640);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(945, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 663);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repositoryItemMemoEdit1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView2;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnFirstFile;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnLineNumber;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumnSecondFile;
+        private System.Windows.Forms.Button button1;
+        private DevExpress.XtraEditors.Repository.RepositoryItemMemoEdit repositoryItemMemoEdit1;
+    }
+}
+
---
+++ VB/WindowsApplication1/Form1.vb
@@ -0,0 +1,94 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Text
+Imports System.Windows.Forms
+Imports System.IO
+Imports DevExpress.XtraEditors
+
+Namespace WindowsApplication1
+	Partial Public Class Form1
+		Inherits Form
+		Public Sub New()
+			InitializeComponent()
+			gridControl1.DataSource = ParseFiles("test1.txt", "test2.txt")
+		End Sub
+
+		Private Function ParseFiles(ByVal path1 As String, ByVal path2 As String) As BindingList(Of LineClass)
+			Dim list As New BindingList(Of LineClass)()
+			Try
+				Dim max As Integer = 0
+				Using sr As New StreamReader(path1)
+					Dim line As String
+					line = sr.ReadLine()
+					Do While line IsNot Nothing
+						list.Add(New LineClass(line, ""))
+						max += 1
+						line = sr.ReadLine()
+					Loop
+				End Using
+
+				Dim i As Integer = 0
+				Using sr As New StreamReader(path2)
+					Dim line As String
+					line = sr.ReadLine()
+					Do While line IsNot Nothing AndAlso i <= max
+						list(i).SecondFileLine = line
+						i += 1
+						line = sr.ReadLine()
+					Loop
+				End Using
+			Catch e As Exception
+				Console.WriteLine("The file could not be read:")
+				Console.WriteLine(e.Message)
+			End Try
+			Return list
+		End Function
+
+		Private Function ValuesAreEqual(ByVal rowHandle As Integer) As Boolean
+			 Dim value1 As Object = gridView1.GetRowCellValue(rowHandle, gridColumnFirstFile)
+			Dim value2 As Object = gridView1.GetRowCellValue(rowHandle, gridColumnSecondFile)
+			Return (Convert.ToString(value1).Equals(Convert.ToString(value2)))
+		End Function
+
+		Private Sub gridView1_RowStyle(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.RowStyleEventArgs) Handles gridView1.RowStyle
+			If (Not ValuesAreEqual(e.RowHandle)) Then
+				e.Appearance.BackColor = Color.Red
+			End If
+		End Sub
+
+		Private Sub Check()
+			For i As Integer = 0 To gridView1.DataRowCount - 1
+				If (Not ValuesAreEqual(i)) Then
+					MessageBox.Show(String.Format("Error in  {0} line:" & Constants.vbLf & "[{1}]" & Constants.vbLf & "[{2}]", i, gridView1.GetRowCellValue(i, gridColumnFirstFile), gridView1.GetRowCellValue(i, gridColumnSecondFile)))
+					gridView1.FocusedRowHandle = i
+					gridView1.FocusedColumn = gridColumnFirstFile
+					gridView1.ShowEditor()
+					If gridView1.ActiveEditor IsNot Nothing Then
+						If XtraMessageBox.Show("Correct this value?", "Question", MessageBoxButtons.YesNo) = System.Windows.Forms.DialogResult.Yes Then
+							gridView1.ActiveEditor.Text = gridView1.GetRowCellValue(i, gridColumnSecondFile).ToString()
+						End If
+						gridView1.ActiveEditor.SelectAll()
+					End If
+					Return
+				End If
+			Next i
+		End Sub
+
+
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			Check()
+		End Sub
+
+		Private Sub gridView1_CustomColumnDisplayText(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.CustomColumnDisplayTextEventArgs) Handles gridView1.CustomColumnDisplayText
+			If e.Column Is gridColumnLineNumber Then
+				e.DisplayText = e.RowHandle.ToString()
+				Return
+			End If
+		End Sub
+
+	End Class
+End Namespace
